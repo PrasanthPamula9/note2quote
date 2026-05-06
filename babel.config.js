eaa34a7739c9e1d76c26e289a6ee41bc99155763
PrasanthPamula9 <prasanthpamula9@gmail.com>
2026-05-06 12:58:55 +0530

    Bug fix

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [["@babel/plugin-proposal-decorators", { "legacy": true }],'react-native-reanimated/plugin']
+  plugins: ['react-native-reanimated/plugin']
 };
